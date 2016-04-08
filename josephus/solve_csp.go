459bf30d5f3c3f310576733da571826ef45b6252
Amit Singh Sethi <amit.pureenergy@gmail.com>
2016-04-08 11:10:30 +0530

    some more changes to the channels implementation

--- josephus/solve_csp.go
+++ josephus/solve_csp.go
@@ -16,8 +16,10 @@ const (
 	PASS action = 1
 )
 
-func run() {
-
+func run(p person, totalAlive chan int, done chan bool) {
+	fmt.Println(p)
+	trackKills(totalAlive)
+	checkOneLeft(totalAlive, done)
 }
 
 func trackKills(totalAlive chan int) {
@@ -26,17 +28,49 @@ func trackKills(totalAlive chan int) {
 	totalAlive <- left
 }
 
-func checkOneLeft(totalAlive chan int) int {
+func checkOneLeft(totalAlive chan int, done chan bool) {
 	left := <-totalAlive
-	return left
+	if left == 1 {
+		done <- true
+	}
 }
 
 func solve(total int) {
-	var done chan person
+	var done chan bool
 	var totalAlive chan int
+	var p person
 	totalAlive <- total
-	p := <-done
+
+	var persons = make([]person, total)
+
+	for i := 0; i < total; i++ {
+		if i == 0 {
+			p = person{
+				pos:   i,
+				alive: true,
+				left:  make(chan action),
+				right: make(chan action),
+			}
+		} else {
+			p = person{
+				pos:   i,
+				alive: true,
+				left:  make(chan action),
+				right: persons[i-1].left,
+			}
+		}
+		persons[i] = p
+
+		persons[0].right = persons[total].left
+	}
+
+	for i := 0; i < total; i++ {
+		go run(persons[i], totalAlive, done)
+	}
+
+	finish := <-done
 	fmt.Println("solve")
+	fmt.Println(finish)
 }
 
 func main() {
